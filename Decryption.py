b8aa53b9f07a4147474796eb8fc703955a030713
Italo Hugo <italohugo625@gmail.com>
2023-11-29 12:49:35 -0300

    New Files

--- Decryption.py
+++ Decryption.py
@@ -1,23 +1,40 @@
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from cryptography.hazmat.primitives import padding as pd
 from Decrypt_File import *
-import secrets
 import pickle
+import sys
 
-key = secrets.token_bytes(32)    # Key used for AES256 256 bits (32 bytes).
-iv = secrets.token_bytes(16)   # Block size for AES 128 bits (16 bytes). The initialization vector needs to be the
-# same size as the block size. The standard block size for AES is 128 bits.
+private_key = loading_pem_file("Private.pem", "123")    # Deserializing the private key and loading
+# into memory.
 
-aes_key = {'key': key, 'iv': iv}
+with open("aes_keys.json", "rb") as file:   # Deserializing the AES Key:
 
-private_key = loading_pem_file("Private.pem", "123")
-
-with open("aes_keys.json", "rb") as file:
-
-    data = file.read()
-    data_bytes = decrypt_data(private_key, data)
-    print(f"Original Data 'Object': {data_bytes}\n")
-    print(f"Encrypted Data 'Object': {data}\n\n")
-    decrypted_data = pickle.loads(data_bytes)
+    data = file.read()  # Loading encrypted AES Key into memory.
+    data_bytes = decrypt_data(private_key, data)    # Decrypting the AES Key with RSA Key.
+    print(f"Original Data 'Object': {data_bytes.hex()}\n")
+    print(f"Encrypted Data 'Object': {data.hex()}\n\n")
+    decrypted_key = pickle.loads(data_bytes)    # Deserializing the AES Key into memory.
 
 
-print(f"Decrypted Key: {decrypted_data['key'].hex()}")
-print(f"Decrypted IV: {decrypted_data['iv'].hex()}\n\n")
+print(f"Decrypted Key: {decrypted_key['key'].hex()}")
+print(f"Decrypted IV: {decrypted_key['iv'].hex()}\n\n")
+
+encrypted_data = reading_file("Screenshot_2023-11-26_13_31_44_(encrypted).png")    # Loading encrypted data into memory.
+
+cipher = Cipher(    # Creating the cipher object.
+    algorithm=algorithms.AES256(key=decrypted_key["key"]),  # Using the decrypted AES Key.
+    mode=modes.CBC(initialization_vector=decrypted_key["iv"])   # Using the decrypted Initialization Vector.
+)
+
+decryptor = cipher.decryptor()  # Creating the decryption object.
+
+padded_message = decryptor.update(encrypted_data) + decryptor.finalize()    # Decrypting the message, which it's still
+# padded.
+
+unpadder = pd.PKCS7(128).unpadder()    # Generating the unpadding object.
+
+decrypted_message = unpadder.update(padded_message) + unpadder.finalize()   # Unpadding the message, generating the full
+# decrypted data.
+
+writing_file("Screenshot_2023-11-26_13_31_44_(decrypted).png", decrypted_message)   # Writing the decrypted data
+# into the original file.
--- Encryption.py
+++ Encryption.py
@@ -6,10 +6,10 @@ import secrets
 import pickle
 import sys
 
-aes_key = {
+aes_key = {    # AES Key.
     "key": secrets.token_bytes(32),    # Key used for AES256 256 bits (32 bytes).
     "iv": secrets.token_bytes(16)   # Block size for AES 128 bits (16 bytes). The initialization vector needs to be the
-# same size as the block size. The standard block size for AES is 128 bits.
+    # same size as the block size. The standard block size for AES is 128 bits.
 }
 
 data_to_be_encrypted = reading_file("Screenshot_2023-11-26_13_31_44.png")
@@ -32,24 +32,26 @@ cipher_obj = Cipher(
 
 encrypted_data = cipher_obj.update(message) + cipher_obj.finalize()    # Executing the encryption process.
 
-writing_files("Screenshot_2023-11-26_13_31_44_(encrypted).png", encrypted_data)
+writing_files("Screenshot_2023-11-26_13_31_44_(encrypted).png", encrypted_data)    # Writing the encrypted data
+# into a file using the original extension.
 
-private_key = generate_private_key()
-public_key = generate_public_key(private_key)
+private_key = generate_private_key()    # Generating the Private Key object.
+public_key = generate_public_key(private_key)   # Generating the Public Key object.
 
-private_pem = generate_private_pem("123", private_key)
-public_pem = generate_public_pem(public_key)
+private_pem = generate_private_pem("123", private_key)  # Generating the Private PEM object.
+public_pem = generate_public_pem(public_key)    # Generating the Public PEM object.
 
-serialization_pem("Private", private_pem)
-serialization_pem("Public", public_pem)
+serialization_pem("Private", private_pem)   # Serializing the Private PEM object.
+serialization_pem("Public", public_pem)    # Serializing the Private PEM object.
 
-with open("aes_keys.json", "wb") as aes_key_file:
+with open("aes_keys.json", "wb") as aes_key_file:   # Serializing the AES Key.
 
-    data_bytes = pickle.dumps(aes_key)
-    print(f"Original Data 'Object': {data_bytes}\n")
-    encrypted_aes_key = encrypt_data(public_key, data_bytes)
-    print(f"Encrypted Data 'Object': {encrypted_aes_key}\n\n")
-    aes_key_file.write(encrypted_aes_key)
+    data_bytes = pickle.dumps(aes_key)  # Generating a byte object of the AES Key dictionary.
+    print(f"Original Data 'Object': {data_bytes.hex()}\n")
+    encrypted_aes_key = encrypt_data(public_key, data_bytes)    # Encrypting the AES Key OBJECT. The data is not
+    # encrypted but the Python object is.
+    print(f"Encrypted Data 'Object': {encrypted_aes_key.hex()}\n\n")
+    aes_key_file.write(encrypted_aes_key)   # Writing the encrypted object into a file, serializing the AES Key.
 
 print(f"Original Key: {aes_key['key'].hex()}")
 print(f"Original IV: {aes_key['iv'].hex()}\n\n")
